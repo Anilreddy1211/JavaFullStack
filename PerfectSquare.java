3c0386ef9db711ba1365724bae0752dd0f99abb7
Anilreddy1211 <116365211+Anilreddy1211@users.noreply.github.com>
2025-07-13 17:04:41 +0530

    PerfectSquare.java

--- PerfectSquare.java
+++ PerfectSquare.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Perfect {
+class Perfect {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         System.out.println("number");
